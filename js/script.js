3a03008147c740350d5c5c31deddb85c80987ad9
Ginda Nugraha Pratama <gindanugrahapratama444@gmail.com>
2024-06-03 13:04:14 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Scroll smooth
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
