a4b48312ff673263912707b39e08ab828b607780
Ginda Nugraha Pratama <gindanugrahapratama444@gmail.com>
2024-06-05 22:14:44 +0700

    add autoslide

--- js/script.js
+++ js/script.js
@@ -23,3 +23,24 @@ document.getElementById('formulir').addEventListener('submit', function(event) {
     document.getElementById('result').value = resultText;
 
 });
+
+let indexSlide = 1;
+showBanner(1);
+
+function nextSlide(n) {
+    showBanner(indexSlide += n);
+}
+
+function showBanner(indexBanner) {
+    let listImage = document.getElementsByClassName('banner-img');
+    if (indexBanner > listImage.length) indexSlide = 1;
+
+    let index = 0;
+    while (index < listImage.length) {
+        listImage[index].style.display = 'none';
+        index++;
+    }
+    listImage[indexSlide - 1].style.display = 'block';
+}
+
+setInterval(() => nextSlide(1), 10000)
